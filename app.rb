58703528189a56a183c12f5d6e051247d53e94f9
Randy Marmer <rmarmer1@yahoo.com>
2016-06-16 10:05:08 +0100

    Challenge 18

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ require_relative 'data_mapper_setup'
 
 
 class BookmarkManager < Sinatra::Base
+
+  # within the body of the Sinatra class
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/links' do
 
 # This uses DataMapper's .all method to fetch all
@@ -45,6 +50,28 @@ class BookmarkManager < Sinatra::Base
     @links = tag ? tag.links : []
     erb :'links/index'
   end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+# save the user id in the session after it's created within app.rb
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+# helper method current_user, that returns an instance of User for the currently logged-in user
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  # post '/users' do
+  #   User.create(email: params[:email], password: params[:password])
+  #   redirect to('/links')
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
